01ead3b2d626c25fcfee3335117817bab42d9d3d
vicentematheus15 <vicentematheus1507@gmail.com>
2026-07-14 14:12:23 -0300

    continuacao

--- pl-sql/procedures.sql
+++ pl-sql/procedures.sql
@@ -1,6 +1,143 @@
+/*PREFIXOS DE VARIÁVEIS
+    const_ : constantes
+    filtro_ : filtros vindos do sistema
+    aux_ : variaveis genéricas que seguram valores auxiliares
+    acc_ : variaveis que serão incrementadas durante a execução do objeto
+    f_ : flags (sim/nao, existe/nao existe)
+    r_ : variaveis que recebem o valor de retorno de uma funcao*/
+    
+    
+-- estrutura padrão quando for criar procedure. criar null e depois alterar o conteúdo dela
 create or replace procedure pcr_exercicios_vicente (
     in_parametro number
 ) is
 begin
     null;
 end;
+
+-- Para chamar ela, mas não executar, abrir segurando o CTRL e clicando
+call pcr_exercicios_vicente();
+
+--Ao abrir, o ambiente será PL/SQL, permitindo usar condicionais, loops e ao finalizar, será compilado, atualizando o conteúdo da procedure
+
+/*1 Criar uma variável do tipo 'NUMBER' chamada w_auxiliar.
+Iniciar essa variável com o valor 10;
+inserir uma linha na tabela xcp_debug com o valor da variável; (é só fazer a chamada
+prc_xcp_debug (1,1,w_auxiliar);*/
+
+create or replace procedure pcr_exercicios_vicente 
+is
+    aux_auxiliar number := 10;
+begin
+    prc_xcp_debug ('teste_variavel_vicente', $$plsql_line, aux_auxiliar);
+end;
+
+call pcr_exercicios_vicente();
+
+select *
+from xcp_debug
+where 0 = 0
+and des_usuario = 'teste_variavel_vicente';
+
+/*2 (UTILIZANDO A CLÁUSULA LOOP)
+Criar uma variável do tipo 'NUMBER' chamada w_auxiliar.
+Iniciar essa variável com o valor 10;
+Criar um laço de interação para enquanto ela for maior que zero, inserir uma linha na
+tabela xcp_debug; (é só fazer a chamada prc_xcp_debug (1,2,w_auxiliar);*/
+
+create or replace procedure pcr_exercicios_vicente is
+    aux_auxiliar number := 10;    
+begin
+    while aux_auxiliar > 0 loop
+         prc_xcp_debug('teste_vicente_loop', $$plsql_line, aux_auxiliar);
+         aux_auxiliar := aux_auxiliar - 1;
+    end loop;
+end;
+
+call pcr_exercicios_vicente();
+
+select *
+from xcp_debug
+where 0 = 0
+and des_usuario = 'teste_vicente_loop'
+
+/*3 (UTILIZANDO A CLAUSA IF() )
+Criar uma variável do tipo 'NUMBER' chamada w_auxiliar.
+Iniciar essa variável com o valor 10;
+Criar um laço de interação para enquanto ela for maior que zero e for número ímpar,
+inserir uma linha na tabela xcp_debug; (é só fazer a chamada prc_xcp_debug
+(1,3,w_auxiliar) */
+
+--sem funcao
+create or replace procedure pcr_exercicios_vicente is
+    aux_auxiliar number := 10;    
+begin
+    while aux_auxiliar > 0 loop
+    if mod(aux_auxiliar, 2) <> 0 then
+         prc_xcp_debug('teste_vicente_loop/if', $$plsql_line, aux_auxiliar);
+    end if;
+         aux_auxiliar := aux_auxiliar - 1;
+    end loop;
+end;
+
+call pcr_exercicios_vicente();
+
+select *
+from xcp_debug
+where 0 = 0
+and des_usuario = 'teste_vicente_loop/if';
+
+
+
+--abstraindo a logica da condicional para uma function
+create or replace procedure pcr_exercicios_vicente is
+    aux_auxiliar number := 10;    
+    -- a function fica na parte das declarações de variaveis, antes do begin da procedure
+    function retorna_num_impar (
+          in_numero number  
+    ) return number
+    is
+    begin
+        return case
+            when mod(in_numero, 2) <> 0 then 1
+            else 0
+        end;
+    end;
+     
+begin
+    while aux_auxiliar > 0 loop
+        if retorna_num_impar(aux_auxiliar) = 1 then
+             prc_xcp_debug('teste_vicente_loop/if_funcao', $$plsql_line, aux_auxiliar);
+        end if;
+    
+         aux_auxiliar := aux_auxiliar - 1;
+    end loop;
+end;
+
+call pcr_exercicios_vicente();
+
+select *
+from xcp_debug
+where 0 = 0
+and des_usuario = 'teste_vicente_loop/if_funcao';
+
+/*4 (UTILIZANDO 'INTO' )
+Criar uma variável do tipo 'NUMBER' chamada w_auxiliar.
+Iniciar essa variável com a quantidade
+de funcionários que existem na folha mensal de julho/2019 utilizando 'select into';
+inserir uma linha na tabela xcp_debug com o valor da variável; (é só fazer a chamada
+prc_xcp_debug (1,4,w_auxiliar);*/
+
+
+/*5 (UTILIZANDO 'CURSOR' )
+Declarar um cursor que busque matricula e nome da tabela de funcionarios;
+Através de um laço de interação, inserir linha a linha a matricula e o nome do
+funcionário na tabela prc_xcp_debug (<nomecursor.matricula>, 5, <nomecursor.nome>)*/
+
+/*6 Faça um loop na tabela inteira de funcionários ( apenas com esse select: select
+matricula, nome, sexo from funcionarios) e escreva na xcp_debug o nome das pessoas
+do sexo feminino*/
+
+/*7 Faça um loop na tabela inteira de funcionarios (apenas assim: select matricula from
+funcionarios) e escreva na tabela xcp_debug o nome do conjuge, se houver. Caso não
+tenha, escreva: NAO TEM*/
